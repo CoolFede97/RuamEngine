a25c7cb1fe83dc10e12a1db698f213f6c282d476
Tomás Atanasoff <tomyatana@gmail.com>
2025-11-03 10:10:12 -0300

    Adding children, untested but didn't break anything

--- RuamEngine/RuamCore/Components/Transform.cpp
+++ RuamEngine/RuamCore/Components/Transform.cpp
@@ -4,8 +4,8 @@ Transform::Transform(const unsigned int obj_id) : Component(obj_id), m_position(
 
 Transform::Transform(const unsigned int obj_id, glm::vec3 pos) : Component(obj_id), m_position(pos) {}
 
-void start() {};
-void update() {};
+void Transform::start() {}
+void Transform::update() {}
 
 void Transform::setPosition(glm::vec3 new_pos) {
 	m_position = new_pos;
@@ -33,4 +33,36 @@ glm::vec3& Transform::scale() {
 
 glm::vec3& Transform::rotation() {
 	return m_rotation;
+}
+
+void Transform::setParent(Transform* parent) {
+	parent->addChild(this);
+}
+
+void Transform::addChild(Transform* child) {
+	auto child_search = std::find(m_children.begin(), m_children.end(), child);
+	if (*child_search != m_children.back()) {
+		throw std::logic_error("Repeated children");
+		return;
+	}
+	m_children.push_back(child);
+}
+
+void Transform::removeChild(unsigned int idx) {
+	auto iter = m_children.begin();
+	std::advance(iter, idx);
+	m_children.erase(iter);
+}
+
+const std::list<Transform*>& Transform::children() const {
+	return m_children;
+}
+
+Transform* Transform::operator[](size_t idx) {
+	if (idx >= m_children.size()) {
+		throw std::out_of_range("Child out of range");
+	}
+	auto iter = m_children.begin();
+	std::advance(iter, idx);
+	return *iter;
 }
--- RuamEngine/RuamCore/Components/Transform.h
+++ RuamEngine/RuamCore/Components/Transform.h
@@ -2,6 +2,8 @@
 
 #include "Component.hpp"
 #include "glm/glm.hpp"
+#include <list>
+
 class Transform : public Component {
 public:
 	using Component::Component;
@@ -9,8 +11,8 @@ public:
 	Transform(unsigned int obj_id);
 	Transform(unsigned int obj_id, glm::vec3 pos);
 
-    void start() {}
-    void update() {}
+	void start();
+	void update();
 
 	void setPosition(glm::vec3 new_pos);
 	void setPosition(float x, float y, float z);
@@ -20,12 +22,20 @@ public:
 	void setScale(glm::vec3 new_pos);
 	void setScale(float x, float y, float z);
 
+	void setParent(Transform* parent);
+	void addChild(Transform* child);
+	void removeChild(unsigned int idx);
+	const std::list<Transform*>& children() const;
+
 	glm::vec3& position();
 	glm::vec3& scale();
 	glm::vec3& rotation();
+
+	Transform* operator[](size_t idx);
 
 protected:
 	glm::vec3 m_position;
 	glm::vec3 m_scale = glm::vec3(1.0f, 1.0f, 1.0f);
 	glm::vec3 m_rotation = glm::vec3(0.0f,0.0f,0.0f);
+	std::list<Transform*> m_children;
 };

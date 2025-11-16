df18226a5083f6bdedcd59c68a27f1a26447c315
Coolfede97 <coolfede97@gmail.com>
2025-11-15 22:27:38 -0300

    compilé con zed!

---
+++ RuamEngine/assets/components/Shooter.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include "Object.hpp"
+#include "../../RuamCore/Input/Input.h"
+#include "RuamTime.h"
+
+#include "glm/glm.hpp"
+
+class Boss : public Component {
+	IMPL_SIMPLE_SERIALIZE(Boss)
+	
+	using Component::Component;
+
+	void start() {
+		if (s_instance == nullptr) {
+			s_instance = this;
+		} else if (s_instance != this) {
+			object()->removeComponent<Boss>(); // ESTO ESTÁ MAL
+		}
+	}
+	
+	void update() 
+	{
+		/*if (m_timeSinceLastShot > m_shootingInterval)
+		{
+			m_timeSinceLastShot = 0;
+			Object* object = SceneManager::ActiveScene()->newObject(0);
+
+			Bullet* bullet = object->addComponent<Bullet>();
+			bullet->speed = 10.0f;
+			bullet->direction = glm::normalize(Shooter::s_instance->object()->transform().position() - object()->transform().position());
+			bullet->radius = 1.0f;
+		} 
+		m_timeSinceLastShot += Time::DeltaTime();*/
+	}
+public:
+	static Boss* s_instance;
+protected:
+	float m_shootingSpeed = 0;
+	float m_shootingInterval = 0;
+	float m_timeSinceLastShot = 0;
+};
+REGISTER_COMPONENT(Boss);
+
+class Bullet : public Component {
+	IMPL_SIMPLE_SERIALIZE(Bullet)
+
+	using Component::Component;
+
+	void start()
+	{
+	}
+	
+	void update() 
+	{
+		object()->transform().setPosition(object()->transform().position() + m_direction * m_speed * Time::DeltaTime());
+	}
+	 
+public:
+	float m_radius;
+	float m_speed;
+	glm::vec3 m_direction;
+private:
+};
+
+REGISTER_COMPONENT(Bullet)
+
+class Shooter : public Component {
+	IMPL_SIMPLE_SERIALIZE(Shooter)
+
+	using Component::Component;
+
+	void start() 
+	{
+		if (s_instance == nullptr) {
+			s_instance = this;
+		}
+		else if (s_instance != this) {
+			object()->removeComponent<Shooter>(); // ESTO ESTÁ MAL
+		}
+	}
+	
+	void update() {
+		if (Input::GetKeyDown(KeyCode::SpaceBar_Key)) {
+			Object* object = SceneManager::ActiveScene()->newObject(0);
+
+			Bullet* bullet = object->addComponent<Bullet>();
+			
+			bullet->object()->transform().position() = s_instance->object()->transform().position();
+
+			bullet->m_speed = m_bulletSpeed;
+			bullet->m_direction = glm::normalize(Boss::s_instance->object()->transform().position() - s_instance->object()->transform().position());
+			bullet->m_radius = m_bulletRadius;
+
+			bullet->object()->addComponent<MeshRenderer>()->SetModel(m_bulletMeshPath);
+		}
+	}
+
+public:
+	Shooter* s_instance = nullptr;
+	float m_bulletSpeed = 0.0f;
+	float m_bulletRadius = 1.0f;
+	std::string m_bulletMeshPath;
+};
+
+REGISTER_COMPONENT(Shooter)
--- RuamEngine/assets/scenes/SandboxScene.cpp
+++ RuamEngine/assets/scenes/SandboxScene.cpp
@@ -4,13 +4,12 @@
 #include "SceneManager.hpp"
 #include "../components/Manager.h"
 #include "../components/Counter.h"
-#include "../components/SandboxCom.h"
-#include "CubeRenderer.h"
 #include "../components/CameraController.h"
-#include "AudioSource.h"
 #include "Serial.hpp"
 #include "MeshRenderer.h"
 #include "GlobalLight.h"
+
+#include "../components/Shooter.h"
 
 void CreateCFSandboxScene()
 {
@@ -23,23 +22,30 @@ void CreateCFSandboxScene()
 	//manager->addComponent<SandboxCom>();
 	//bag->transform().setRotation(glm::vec3(45.0f, 5.0f, 45.0f));
 	//bag->addComponent<CubeRenderer>()->materialId = 0;
-	
-	Object* bag2 = sandboxScene->newObject();
-	bag2->transform().setPosition(glm::vec3(0.0f, -1.5f, 7.0f));
-	bag2->addComponent<MeshRenderer>()->SetModel("assets/meshes/boss/boss.obj");
+
+	Object* boss = sandboxScene->newObject();
+	boss->transform().setPosition(glm::vec3(0.0f, -1.5f, 7.0f));
+	boss->addComponent<MeshRenderer>()->SetModel("assets/meshes/boss/boss.obj");
 	//bag2->addComponent<CubeRenderer>()->materialId = 0;
-	
-	Object* bag = sandboxScene->newObject();
-	bag->addComponent<MeshRenderer>()->SetModel("assets/meshes/skyboxes/galaxy/galaxySkybox.obj");
-	bag->getComponent<MeshRenderer>()->m_model->m_meshes[0].m_material->m_shininess = 1000.0;
-	bag->transform().setRotation(glm::vec3(180.0f, 0.0, 0.0));
-	bag->transform().setScale(glm::vec3(500.0f, 500.0f, 500.0f));
+
+	Object* skybox = sandboxScene->newObject();
+	skybox->addComponent<MeshRenderer>()->SetModel("assets/meshes/skyboxes/galaxy/galaxySkybox.obj");
+	skybox->getComponent<MeshRenderer>()->m_model->m_meshes[0].m_material->m_shininess = 1000.0;
+	skybox->transform().setRotation(glm::vec3(180.0f, 0.0, 0.0));
+	skybox->transform().setScale(glm::vec3(500.0f, 500.0f, 500.0f));
+
+	Object* player = sandboxScene->newObject();
+	player->addComponent<CameraController>();
+	player->getComponent<CameraController>()->m_speed = 10.5f;
+	player->getComponent<CameraController>()->m_rotationSpeed = 80.0f;
+	player->addComponent<Shooter>();
+
+	Shooter* shooter = player->getComponent<Shooter>();
+	shooter->m_bulletSpeed = 10.0f;
+	shooter->m_bulletMeshPath = "assets/meshes/boss/boss.obj";
 
 	Object* manager = sandboxScene->newObject();
 	manager->addComponent<Camera>();
-	manager->addComponent<CameraController>();
-	manager->getComponent<CameraController>()->m_speed = 10.5f;
-	manager->getComponent<CameraController>()->m_rotationSpeed = 80.0f;
 	//manager->addComponent<CameraController>();
 	manager->addComponent<Counter>();
 	manager->addComponent<Manager>();
